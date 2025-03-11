b40172643d0158034f5f514b1cd587aee58e92a8
Ivan Lanovskyi <lanowsiwan@gmail.com>
2025-03-11 21:37:22 +0200

    OnLogin fixed

--- WeatherAppFrontend/src/app/login/login.component.ts
+++ WeatherAppFrontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Credentials } from '../models/credentials.model';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     this.loginForm = new FormGroup(
       {
         email: new FormControl("", [Validators.email, Validators.required]),
@@ -22,6 +24,7 @@ export class LoginComponent {
   onLogin() {
     var email = this.loginForm.controls['email'].value;
     var password = this.loginForm.controls['password'].value;
+    this.authService.login({ email, password });
   }
 }
 
